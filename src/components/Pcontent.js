e8b5fc09afdfc400aca32819fbd0e15e3ba693a8
sunzhaoqkd <sunzhaoqkd@163.com>
2019-09-12 11:31:48 +0800

    update

--- src/components/Pcontent.js
+++ src/components/Pcontent.js
@@ -1,6 +1,6 @@
 import React , {Component} from 'react';
 import axios from 'axios';
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 class Pcontent extends Component {
     constructor(props) {
         super(props);
@@ -27,29 +27,22 @@ class Pcontent extends Component {
     render() {
         return (
             <div className="pcontent"> 
-                <div className="back">返回</div>
+                <div className="back">
+                    <Link to='/'>返回</Link>
+                </div>
                 
                 <div className="p_content">		
-                    <div className="p_info">				
-                        <img src="images/product.jpg"/>				
+                    <div className="p_info">	
+                        {this.state.list.img_url? <img src={`${this.state.domain}${this.state.list.img_url}`}/>	:''}						
                         <h2>{this.state.list.title}</h2>				
-                        <p className="price">22.00/份</p>
+                        <p className="price">{this.state.list.price}/份</p>
                     </div>
                     <div className="p_detial">
                         <h3>
                             商品详情					
                         </h3>
-                        <div className="p_content"> 
-                            <img src="images/product.jpg"/>
-                            <br />
-                            <p>
-                                韩国辣酱海鲜炒面,青椒炒牛肉,芦笋腰果炒虾仁,『家常料理』简单又好吃的辣炒起司年糕鸡排
-                            </p>
-                            
-                            <br />
-                            <p>
-                                韩国辣酱海鲜炒面,青椒炒牛肉,芦笋腰果炒虾仁,『家常料理』简单又好吃的辣炒起司年糕鸡排
-                            </p>
+                        <div className="p_content" dangerouslySetInnerHTML={{__html: this.state.list.content}} > 
+
                         </div>
                     </div>
                 </div>
@@ -62,7 +55,7 @@ class Pcontent extends Component {
                         <div className="cart_num">
                         <div className="input_left">-</div>
                         <div className="input_center">
-                            <input type="text"  readonly="readonly" value="1" name="num" id="num" />
+                            {/* <input type="text"  readonly="readonly" value="1" name="num" id="num" /> */}
                         </div>
                         <div className="input_right">+</div>				      
                         </div>								
